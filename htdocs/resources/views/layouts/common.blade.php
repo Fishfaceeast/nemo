d0e4642ba1d22843b2635fac5b8c4d98a08c7bd9
YuQian <yuqian@baixing.com>
2016-05-28 17:31:15 +0800

    modal 交互

--- htdocs/resources/views/layouts/common.blade.php
+++ htdocs/resources/views/layouts/common.blade.php
@@ -61,6 +61,7 @@
 
         @section('scripts')
             <script src="/node_modules/jquery/dist/jquery.min.js"></script>
+            <script src="/node_modules/underscore/underscore-min.js"></script>
             <script src="/node_modules/tether/dist/js/tether.min.js"></script>
             <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
             <script src="/d/profile/index.js"></script>
--- htdocs/resources/views/profile/index.blade.php
+++ htdocs/resources/views/profile/index.blade.php
@@ -21,7 +21,7 @@
                         <i class="fa fa-pencil" aria-hidden="true"></i>
                     </h5>
                     @foreach ($basics as $basic)
-                        <p>{{ $basic->gender }} • {{ $basic->city }} • {{ $basic->birth_year }}</p>
+                        <p class="info-content">{{ $basic->gender }} • {{ $basic->city }} • {{ $basic->birth_year }}</p>
                     @endforeach
                 </div>
             </div>
@@ -104,27 +104,27 @@
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
-                        <h4 class="modal-title">Modal title</h4>
+                        <h4 class="modal-title">你的基本信息</h4>
                     </div>
                     <div class="modal-body">
-                        <form id="basicInfo" action="/basic/update" method="POST">
-                            <label for="gender">性别
+                        <form id="basicForm" action="/basic/update" method="POST">
+                            <div class="form-group">
+                                <label for="gender">性别</label>
                                 <input type="text" name="gender"/>
-                            </label>
-                            <label for="city">城市
+                            </div>
+                            <div class="form-group">
+                                <label for="city">城市</label>
                                 <input type="text" name="city"/>
-                            </label>
-                            <label for="birth_year">出生年
+                            </div>
+                            <div class="form-group">
+                                <label for="birth_year">出生年</label>
                                 <input type="text" name="birth_year"/>
-                            </label>
-                            <button type="submit" class="btn btn-primary">
-                                <i class="fa fa-btn fa-user"></i>Modify
-                            </button>
+                            </div>
                         </form>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-primary basic-modify">保存设置</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
                     </div>
                 </div><!-- /.modal-content -->
             </div><!-- /.modal-dialog -->
