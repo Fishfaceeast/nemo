99dd41b15bde253b83bc3f5b1130ff421b563a9e
YuQian <yuqian@baixing.com>
2016-06-21 23:02:28 +0800

    naive pop-over

---
+++ htdocs/public/js/match/index.js
@@ -0,0 +1,5 @@
+import Validator from '../module/validator.js'
+
+$('.pop-switch').on('click', function() {
+	$(this).parent().toggleClass('open')
+})
--- htdocs/resources/views/match/index.blade.php
+++ htdocs/resources/views/match/index.blade.php
@@ -10,22 +10,27 @@
     <div class="main-container clearfix">
         <section class="base-search">
             <div>
-                搜一下：男，
-                <span class="pop-form">
-                    <label>
-                        <input type="checkbox" val="男"/> 男
-                    </label>
-                    <label>
-                        <input type="checkbox" val="女"/> 女
-                    </label>
-                    <label>
-                        <input type="checkbox" val="所有人"/> 所有人
-                    </label>
-                </span>
+                <strong>
+                    <i class="pop-switch">
+                        搜一下：男，
+                    </i>
+                    <span class="arrow-box pop-over">
+                        <h4>性别</h4>
+                        <label>
+                            <input type="checkbox" val="男"/> 男
+                        </label>
+                        <label>
+                            <input type="checkbox" val="女"/> 女
+                        </label>
+                        <label>
+                            <input type="checkbox" val="所有人"/> 所有人
+                        </label>
+                    </span>
+                </strong>
+                <strong>年龄25-35，</strong>
+                <strong>对女性感兴趣，</strong>
+                <strong>坐标上海。</strong>
             </div>
-            <div>年龄25-35，</div>
-            <div>对女性感兴趣，</div>
-            <div>坐标上海。</div>
         </section>
         <section class="advanced-search">
 
@@ -35,5 +40,7 @@
 
 @section('scripts')
     @parent
+
+    <script src="/d/match/index.js"></script>
 
 @endsection
