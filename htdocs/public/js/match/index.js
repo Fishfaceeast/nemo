869c743f48201966aaacc1e1df257cbffa0ac03b
YuQian <yuqian@baixing.com>
2016-06-30 22:44:25 +0800

    adv panel && cover native

--- htdocs/public/js/match/index.js
+++ htdocs/public/js/match/index.js
@@ -1,9 +1,14 @@
 import Validator from '../module/validator.js'
 
 const MATCH_URL = '/match/search'
+
 var $board = $('.board')
-var $baseForm = $('#base-form')
 var $panel = $('.adv-feature')
+var $cover = $('#cover')
+
+var baseData = {}
+var advanceData = {}
+var advKey = []
 
 $('.pop-over').on('click', function(e) {
 	e.stopPropagation()
@@ -16,8 +21,7 @@ $('.pop-switch').on('click', function(e) {
 })
 $('body').on('click', function(e) {
 	if($(this).hasClass('pop-open')) {
-		let data = $baseForm.serialize()
-		$.post(MATCH_URL, data, function(res) {
+		$.post(MATCH_URL, baseData, function(res) {
 			if(res.data) {
 				makeCard(res.data)
 			}
@@ -29,7 +33,6 @@ $('body').on('click', function(e) {
 
 const makeCard = (items) => {
 	let str = ''
-	console.log(items)
 	let d = new Date();
 	let y = d.getFullYear();
 	$.each(items, function(id, obj) {
@@ -42,50 +45,123 @@ const makeCard = (items) => {
 const makePanel = (arr) => {
 	let str = ''
 	$.each(arr, function(key, value) {
-		str += `<strong>${value}</strong>`
+		str += `<strong>${value}<span class="clearIt" data-key="${value}"> &times; </span></strong>`
 	})
 	$panel.html(str)
 }
 
-$('.choice-block').on('click', function(e) {
+$('.base-choice').on('click', function(e) {
+	let $target = $(e.target)
+	let key = $target.parent().data('key')
+	let val = $target.data('value')
+
+
+	if(!$target.hasClass('active')) {
+		baseData[key] = val
+		$target.siblings().removeClass('active')
+	}
+	$target.toggleClass('active')
+
+})
+$('#base-form input').on('change', function(e) {
+	let key = $(this).parent().data('key')
+	let val = $(this).val()
+
+	if(val) {
+		baseData[key] = val
+	} else {
+		delete baseData[key]
+	}
+})
+
+$('.adv-choice').on('click', function(e) {
 	let $target = $(e.target)
 	let val = $target.data('value')
+	let k = $target.parent().data('key')
+
+	if($target.hasClass('active')) {
+		advKey = _.without(advKey,k)
+		delete advanceData[k]
+	} else {
+		advKey.push(k)
+		advanceData[k] = val
+	}
 	$target.toggleClass('active')
-	$target.siblings('input').data('value', val)
+
+})
+
+$('.advanced-search select').on('change', function() {
+	let key = $(this).parent().data('key')
+	let val = $(this).val()
+
+	if(val) {
+		advanceData[key] = val
+		advKey.push(key)
+	} else {
+		advKey = _.without(advKey,key)
+		delete advanceData[key]
+	}
 })
 
 $('.btn-search').on('click', function(e) {
-	let $target = $(e.target)
-	let $inputs = $target.siblings('div').children('input')
-	let advancedData = {}
 
-	$inputs.each(function(index) {
-		let k = $(this).data('key')
-		let v = $(this).data('value')
-		if(v) {
-			advancedData[k] = v
-		}
-	})
-
-	let keys = _.keys(advancedData)
-	if(keys.length > 0) {
-		let baseData = $baseForm.serialize()
-		console.log(baseData)
-		console.log(advancedData)
-		//let data = $.extend(true, advancedData, baseData)
-		$.post(MATCH_URL, advancedData, function(res) {
+	if(advKey.length > 0) {
+		let data = $.extend(true, advanceData, baseData)
+		$.post(MATCH_URL, data, function(res) {
 			if(res.data){
 				makeCard(res.data)
 			}
 		})
-		makePanel(keys)
-	} else {
-		$('.advanced-search').hide()
+		makePanel(advKey)
 	}
+	$('.advanced-search').hide()
+
 })
 
 $('.btn-cancel').on('click', function() {
 	$('.advanced-search').hide()
 })
 
+$('.fa-sliders').on('click', function() {
+	$('.advanced-search').toggle()
+	$cover.show()
+})
+
+$('.close-cover').on('click', function() {
+	$cover.hide()
+	$('.advanced-search').toggle()
+})
+
+$('.adv-feature').on('click', '.clearIt', function(e) {
+	let key = $(e.target).data('key')
+	init(key)
+	delete advanceData[key]
+	advKey = _.without(advKey,key)
+	let data = $.extend(true, advanceData, baseData)
+	$.post(MATCH_URL, data, function(res) {
+		if(res.data){
+			makeCard(res.data)
+		}
+	})
+	makePanel(advKey)
+})
+
+var init = function(key) {
+	let $sel = $('.tab-pane > div').filter(function() {
+		return $(this).data('key') == key;
+	})
+	let type = $sel.data('type')
+
+	switch (type) {
+		case 'select':
+			$sel.find('option:nth-child(1)').attr('selected', 'selected')
+			break;
+		case 'checkbox':
+			console.log('checkbox coming soon')
+			break;
+		default:
+			$sel.find('.choice-block').removeClass('active')
+	}
+
+}
 
